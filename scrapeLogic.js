bf1959d9312047005a534c842e0023702c2ea5ee
Bruno Negri <b.negri_@hotmail.com>
2024-10-10 17:57:45 -0300

    update

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -8,6 +8,7 @@ const scrapeLogic = async (res) => {
       "--no-sandbox",
       "--single-process",
       "--no-zygote",
+      "--start-maximized"
     ],
     executablePath:
       process.env.NODE_ENV === "production"
@@ -16,35 +17,20 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
-
-    await page.goto("https://developer.chrome.com/");
-
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    await page.goto('https://friasneto.reportload.com/signin'); // wait until page load
+    page.waitForNavigation({ waitUntil: 'networkidle0' })
+    await page.waitForSelector('input[type="email"]', { visible: true })
+    await page.type('input[type="email"]', uLogin)
+    await page.waitForSelector('input[type="password"]', { visible: true })
+    await page.type('input[type="password"]', uPass)
+    await Promise.all([
+        page.click('button[type="submit"]'),
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    ]);
+res.send('função deu certo')
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
   }
 };
 
